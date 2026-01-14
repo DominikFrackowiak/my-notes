ea3f3fd4a1a835f9153abc87a35e5f7a445cf6d6
DominikFrackowiak <rorschach@op.pl>
2026-01-14 09:46:25 +0100

    feat/rich-tex-editor: main structure and functionality finished, styling to be done, image uploading does not work

--- src/components/organisms/rich-text-editor/index.tsx
+++ src/components/organisms/rich-text-editor/index.tsx
@@ -1,33 +1,109 @@
-// src/Tiptap.tsx
-import { useEditor, EditorContent } from '@tiptap/react'
-import Placeholder from '@tiptap/extension-placeholder'
-import StarterKit from '@tiptap/starter-kit'
-import Underline from '@tiptap/extension-underline'
-import TextFormattingMenu from './text-formatting-menu'
+import { useEditor, EditorContent } from "@tiptap/react";
+import Placeholder from "@tiptap/extension-placeholder";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import Image from "@tiptap/extension-image";
+
+// import { ImageUploadNode } from "@/components/tiptap-node/image-upload-node";
+// import { handleImageUpload, MAX_FILE_SIZE } from "@/lib/tiptap-utils";
+
+import { useRef } from "react";
+import MainOptionsMenu from "./main-options-menu";
+import { useFormStore } from "../../../store/ui/form.store";
+import { cn } from "../../../lib/tw-merge";
+import { backgroundColors } from "../../../constants/background-colors";
+import { useNoteDraftStore } from "../../../store/note-draft/note-draft.store";
 
 function RichTextEditor() {
-	const editor = useEditor({
-		extensions: [
-			StarterKit,
-			Underline,
-			Placeholder.configure({
-				placeholder: 'Write something ...',
-			}),
-		],
-		// content: '<p>Utworz notatke</p>',
-		editorProps: {
-			attributes: {
-				class: 'tiptap bg-gray-50 min-h-10 min-w-50',
-			},
-		},
-	})
+  const refEditor = useRef<HTMLDivElement | null>(null);
+  const isFormActive = useFormStore((store) => store.isFormActive);
+  const setIsFormActive = useFormStore((store) => store.setIsFormActive);
+  const backgroundColor = useNoteDraftStore(
+    (store) => store.note.backgroundColor
+  );
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Placeholder.configure({
+        placeholder: "Utwórz notatkę…",
+      }),
+      Image.configure({
+        resize: {
+          enabled: true,
+          alwaysPreserveAspectRatio: true,
+        },
+      }),
+      //   ImageUploadNode.configure({
+      //     accept: "image/*",
+      //     maxSize: MAX_FILE_SIZE,
+      //     limit: 3,
+      //     upload: handleImageUpload,
+      //     onError: (error) => console.error("Upload failed:", error),
+      //   }),
+    ],
+    // content: '<p>Utworz notatke</p>',
+    editorProps: {
+      attributes: {
+        class: "tiptap",
+      },
+    },
+  });
 
-	return (
-		<>
-			<EditorContent editor={editor} />
-			<TextFormattingMenu editor={editor} />
-		</>
-	)
+  const titleEditor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Placeholder.configure({
+        placeholder: "Tytuł",
+      }),
+    ],
+    // content: '<p>Utworz notatke</p>',
+    editorProps: {
+      attributes: {
+        class: "tiptap",
+      },
+    },
+  });
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div
+      className={cn(
+        "flex justify-center items-start",
+        isFormActive
+          ? "h-screen w-full bg-transparent"
+          : "h-11 w-auto bg-transparent"
+      )}
+      onClick={(e) => {
+        e.stopPropagation();
+        setIsFormActive(false);
+      }}
+    >
+      <div
+        data-ui="form"
+        ref={refEditor}
+        className={cn(
+          "flex flex-col w-full lg:w-form shadow-xl rounded-lg border border-border-form transition-colors duration-1000 text-txt-translucid gap-4",
+          !isFormActive
+            ? "h-11 justify-center px-4"
+            : "min-h-34 h-auto justify-start px-4 pt-3",
+          backgroundColors[backgroundColor]
+        )}
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsFormActive(true);
+        }}
+      >
+        {isFormActive ? <EditorContent editor={titleEditor} /> : null}
+        <EditorContent editor={editor} />
+        {isFormActive ? <MainOptionsMenu editor={editor} /> : null}
+      </div>
+    </div>
+  );
 }
 
-export default RichTextEditor
+export default RichTextEditor;
---
+++ src/components/organisms/rich-text-editor/main-options-menu.tsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+
+// import useImageUpload from "@/components/tiptap-ui/image-upload-button";
+import Button from "../../atoms/Button";
+import PopoverComponent from "../../molecules/popover";
+// import ImageIcon from "../../assets/icons/image.svg?react";
+import ImageIcon from "../../../assets/icons/image.svg?react";
+import PaletteIcon from "../../../assets/icons/palette.svg?react";
+import FormattingIcon from "../../../assets/icons/formatting.svg?react";
+import MoreOptionsIcon from "../../../assets/icons/more-options.svg?react";
+import ArchiveIcon from "../../../assets/icons/archive.svg?react";
+
+import BackgroundColorSelection from "../form-create-note/components/background-color-selection";
+import MoreOptions from "../form-create-note/components/more-options";
+import TextFormattingMenu from "./text-formatting-menu";
+import { type Editor } from "@tiptap/react";
+import { useFormStore } from "../../../store/ui/form.store";
+import { useNoteDraftStore } from "../../../store/note-draft/note-draft.store";
+
+export default function MainOptionsMenu({ editor }: { editor: Editor }) {
+  // const { handleImage } = useImageUpload({
+  //   editor,
+  //   hideWhenUnavailable: true,
+  //   onInserted: () => {
+  //     // opcjonalnie: toast / analytics
+  //   },
+  // });
+  const [isTextFormattingMenuOpen, setIsTextFormattingMenuOpen] =
+    useState(false);
+  const [showCreatableTagSelect, setShowCreatableTagSelect] =
+    useState<boolean>(false);
+
+  const setIsFormActive = useFormStore((store) => store.setIsFormActive);
+  const setIsArchived = useNoteDraftStore((store) => store.setIsArchived);
+
+  //   const fileRef = useRef<HTMLInputElement | null>(null);
+  return (
+    <div className="w-full flex flex-col gap-2 p-2 border border-red-700">
+      {isTextFormattingMenuOpen ? <TextFormattingMenu editor={editor} /> : null}
+      <div className="flex w-full justify-between items-center">
+        <div className="w-full flex items-center gap-2">
+          <Button
+            size="SM"
+            icon={<FormattingIcon className=" text-txt dark:text-txt-dark" />}
+            onClick={() => setIsTextFormattingMenuOpen((v) => !v)}
+          />
+          <PopoverComponent
+            trigger={
+              <Button
+                size="SM"
+                icon={<PaletteIcon className="text-txt dark:text-txt-dark" />}
+              />
+            }
+            onClick={() => setIsTextFormattingMenuOpen(false)}
+          >
+            <BackgroundColorSelection />
+          </PopoverComponent>
+          <Button
+            size="SM"
+            icon={<ImageIcon className=" text-txt dark:text-txt-dark" />}
+            onClick={(e) => {
+              e.stopPropagation();
+              //   fileRef.current?.click();
+              alert("working");
+              // handleImage();
+            }}
+          />
+
+          <Button
+            size="SM"
+            icon={<ArchiveIcon className=" text-txt dark:text-txt-dark" />}
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsArchived(true);
+              setIsFormActive(false);
+            }}
+          />
+          <PopoverComponent
+            trigger={
+              <Button
+                size="SM"
+                icon={
+                  <MoreOptionsIcon className="text-txt dark:text-txt-dark" />
+                }
+              />
+            }
+            onClick={() => {
+              setShowCreatableTagSelect(false);
+              setIsTextFormattingMenuOpen(false);
+            }}
+          >
+            <MoreOptions
+              showCreatableTagSelect={showCreatableTagSelect}
+              setShowCreatableTagSelect={setShowCreatableTagSelect}
+              data-ui="form-more-options"
+            />
+          </PopoverComponent>
+        </div>
+        <Button
+          isRectangular={true}
+          onClick={(e) => {
+            e.stopPropagation();
+
+            setIsFormActive(false);
+          }}
+        >
+          <span className="text-txt-translucid-darker text-sm dark:text-txt-dark">
+            Zamknij
+          </span>
+        </Button>
+      </div>
+    </div>
+  );
+}
